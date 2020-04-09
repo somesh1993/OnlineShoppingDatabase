c7330d81833573fd4d188114feca903936401979
Somesh Nigam <37415800+somesh1993@users.noreply.github.com>
2020-04-09 14:09:30 +0530

    Add files via upload

---
+++ OnlineDatabase/dbo/Tables/ShoppingCard.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[ShoppingCard](
+	[BankName] [nvarchar](50) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](50) NOT NULL,
+	[CardNumber] [nvarchar](50) NOT NULL,
+	[ValidDate] [nvarchar](50) NOT NULL,
+	[Price] [nvarchar](50) NOT NULL, 
+    CONSTRAINT [PK_ShoppingCard] PRIMARY KEY ([CardNumber])
+) ON [PRIMARY]
